8858059a8ffd4492d189790be08cfc1ec69451d3
Archanaghadage <ghadagearchana32@gmail.com>
2025-08-15 12:52:22 +0530

    DB+CORS

--- employee/src/main/java/com/codeWithProject/CrossOrigin/CorsConfig.java
+++ employee/src/main/java/com/codeWithProject/CrossOrigin/CorsConfig.java
@@ -1,26 +1,26 @@
 package com.codeWithProject.CrossOrigin;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class CorsConfig {
 
-    private final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    @Value("${ALLOWED_ORIGINS:http://localhost:3000}")
+    private String allowedOriginsEnv;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        String allowedOriginsEnv = dotenv.get("ALLOWED_ORIGINS", "http://localhost:3000");
         String[] allowedOrigins = allowedOriginsEnv.split(",");
 
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(allowedOrigins) // multiple origins from .env
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
